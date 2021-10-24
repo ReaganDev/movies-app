d2c677d3ee4ac2ae36a987742c21f16a16e88de4
Reagan Reuben <reagan.reuben@decagon.dev>
2021-10-24 14:39:30 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import Movielist from './components/Movielist';
 
 ReactDOM.render(<Movielist />,document.getElementById('root'));
